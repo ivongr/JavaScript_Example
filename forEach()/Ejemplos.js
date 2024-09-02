5ae87fa90773e114989ccbe2241f6257b2f1f293
ivongr <itzelivongr@gmail.com>
2024-09-02 11:37:33 -0600

    feat: Agregar ejemplo numberArray

--- forEach()/Ejemplos.js
+++ forEach()/Ejemplos.js
@@ -12,3 +12,15 @@ var sum = imitarForEach(function(elemento){
 },[1,2,3,4]);
 
 sum;
+
+//2
+let numberArray = [1,2,3,4,5];
+
+let returnValue = 
+numberArray.forEach(num =>
+    console.log(`${num} x ${num} = ${num * num}`));
+
+//console.log(numberArray);
+console.log(returnValue);
+
+    

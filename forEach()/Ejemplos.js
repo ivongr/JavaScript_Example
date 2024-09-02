99126192f475d837bb07f20775a0d3127191b15a
ivongr <itzelivongr@gmail.com>
2024-09-02 10:57:31 -0600

    feat: Agregar function imitarForEach

--- forEach()/Ejemplos.js
+++ forEach()/Ejemplos.js
@@ -1,5 +1,14 @@
+//No devuelve ningun error
 function imitarForEach(funct,elementos){
     for(let i = 0; i < elementos.length; i++){
         funct(elementos[i]);
     }
 }
+
+var sum = imitarForEach(function(elemento){
+    var addOne = elemento + 1;
+    console.log(addOne);
+    return addOne; //la funcion argumento tiene un return
+},[1,2,3,4]);
+
+sum;

0bbf7cea6716462694413013a0d7347a57a09e6e
ivongr <itzelivongr@gmail.com>
2024-09-02 16:28:35 -0600

    feat: Agregar ejemplo de let,var,const

--- forEach()/Ejemplos.js
+++ forEach()/Ejemplos.js
@@ -18,7 +18,7 @@ let numberArray = [1,2,3,4,5];
 
 let returnValue = 
 numberArray.forEach(num =>
-    console.log(`${num} x ${num} = ${num * num}`));
+console.log(`${num} x ${num} = ${num * num}`));
 
 console.log(numberArray);
 console.log(returnValue);
--- map()/Ejemplos_map().js
+++ map()/Ejemplos_map().js
@@ -24,3 +24,16 @@ let returnValue = numberArray.map
 
 console.log(numberArray);
 console.log(returnValue);
+
+//encandemiento de map
+//puedes llamar una serie de operaciones
+
+let numberArrayEnca = [1,2,3,4,5];
+
+let returnValueEn = numberArray.map((num) => num * 2)
+//convertir en numero a string
+.map((num) => num.toString())
+.map((string) => "$"+string);
+
+console.log(numberArrayEnca);
+console.log(returnValueEn);
